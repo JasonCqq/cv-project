9a6b6fde4a03dd1b789290af57c60b9ae9bb0114
Jason <121495300+jason21715@users.noreply.github.com>
2023-05-16 01:56:41 -0700

    Add form skeleton

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import '../src/styles/App.css';
 import React, { Component } from 'react';
 
 class App extends Component {
@@ -9,7 +9,53 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-  
+        <form className="cv-form">
+          <section className='general-information'>
+            <div> 
+              <label for="nameInput">Name</label>
+              <input id="nameInput" name="name" type="text"></input>
+            </div>
+
+            <div>
+              <label for="email">Email</label>
+              <input id='emailInput' name="email" type="email"></input>
+            </div>
+
+              <label for="phone">Phone Number</label>
+              <input id="phoneInput" name="phone" type="tel"></input>
+              <small>Format: 123-456-7890</small>
+          </section>
+
+          <section className='educational-information'>
+            <label for="schoolName">School Name</label>
+            <input id="schoolInput" name="schoolName" type="text"></input>
+
+            <label>Title of Study</label>
+            <input type="text"></input>
+
+            <label>Date of Study</label>
+            <input></input>
+          </section>
+
+          <section className='work-information'>
+            <label>Company Name</label>
+            <input type="text"></input>
+
+            <label>Position Title</label>
+            <input type="text"></input>
+
+            <label>Description of your contributions</label>
+            <textarea type="text"></textarea>
+
+            <label>From</label>
+            <input></input>
+
+            <label>Until</label>
+            <input></input>
+          </section>
+
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
